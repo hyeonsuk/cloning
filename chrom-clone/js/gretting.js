45360ed3b76b1e75dc5241d702bf95b943448b08
hyeonsuk <qu3230@gmail.com>
2019-10-10 22:58:35 +0900

    clock, greeting까지 완료

--- chrom-clone/js/gretting.js
+++ chrom-clone/js/gretting.js
@@ -1,5 +1,5 @@
-const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
+const nameForm = document.querySelector(".js-nameForm"),
+    input = nameForm.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
@@ -17,12 +17,12 @@ function handleSubmit(event){
 }
 
 function askForName() {
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
+    nameForm.classList.add(SHOWING_CN);
+    nameForm.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN);
+    nameForm.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello, ${text}`;
 }
