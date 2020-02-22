597fea57dec88ce0330fc7e0cef6af18f0df42e6
hyeonsook95 <qu3230@gmail.com>
2020-02-23 05:43:23 +0900

    #3 #4 완료

---
+++ blockchain-typescript/src/index.ts
@@ -0,0 +1,8 @@
+
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, ${age}, ${gender}`;
+}
+
+console.log(sayHi("hyeonsook", 26, "female"));
+
+export { };
